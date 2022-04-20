1e4c5350e00d7f6811a06624576b4dc46e82dac0
SvenRasmusen <sven@home.lu>
2022-04-20 21:49:49 +0200

    added login screen

--- lib/main.dart
+++ lib/main.dart
@@ -32,36 +32,157 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
-  int _counter = 0;
+  //FOCUS NODES
+  final FocusNode _loginEmailFocus = FocusNode();
+  final FocusNode _loginPasswordFocus = FocusNode();
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+  //TEXTFIELD CONTROLLERS
+  TextEditingController loginEmailController = TextEditingController();
+  TextEditingController loginPasswordController = TextEditingController();
+
+  //KEYS
+  final GlobalKey<FormState> _loginFormKey = GlobalKey<FormState>();
+
+  bool rememberMe = false;
 
   @override
   Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
+      body: Container(
+        child: Center(
+            child: Row(
+            children: [
+              Container(
+                height: height,
+                width: width / 2,
+                child: Center(child: loginForm()),
+              ),
+              Container(
+                height: height,
+                width: width / 2,
+                color: const Color(0xFF0A150F),
+                child: Image.asset("assets/images/loginImage.png",
+                    fit: BoxFit.fitHeight),
+              )
+            ],
+        )),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  Widget loginForm() {
+    return Form(
+        key: _loginFormKey,
+        child: SizedBox(
+            width: 400,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text(
+                  "Hello, Welcome Back!",
+                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(height: 50),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text("Email Address",
+                      textAlign: TextAlign.left,
+                      style: TextStyle(color: Colors.grey)),
+                ),
+                SizedBox(height: 5),
+                TextFormField(
+                  controller: loginEmailController,
+                  decoration: const InputDecoration(
+                      labelText: "Enter your email",
+                      suffixIcon: Icon(Icons.email_outlined),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(7))),
+                      errorBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.red, width: 5))),
+                  validator: (value) {
+                    if (value == null || value.trim().isEmpty) {
+                      return "Please enter a valid email address.";
+                    }
+                  },
+                  textAlign: TextAlign.left,
+                ),
+                SizedBox(height: 25),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text("Password",
+                      textAlign: TextAlign.left,
+                      style: TextStyle(color: Colors.grey)),
+                ),
+                SizedBox(height: 5),
+                TextFormField(
+                  controller: loginEmailController,
+                  decoration: const InputDecoration(
+                      labelText: "Enter your password",
+                      suffixIcon: Icon(Icons.vpn_key),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(7))),
+                      errorBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.red, width: 5))),
+                  validator: (value) {
+                    if (value == null || value.trim().isEmpty) {
+                      return "Please enter your password.";
+                    }
+                  },
+                  textAlign: TextAlign.left,
+                ),
+                SizedBox(height: 25),
+                Container(
+                  child: Row(
+                    children: [
+                      Checkbox(
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(4)),
+                        activeColor: Colors.green,
+                        value: rememberMe,
+                        onChanged: (value) => {
+                          setState(() {
+                            rememberMe = !rememberMe;
+                          })
+                        },
+                      ),
+                      Text("Remember me", style: TextStyle(color: Colors.grey)),
+                      Spacer(),
+                      GestureDetector(
+                        child: Text("Forgot Password?",
+                            style: TextStyle(fontWeight: FontWeight.bold)),
+                        onTap: () => print("Clicked forgot password."),
+                      )
+                    ],
+                  ),
+                ),
+                SizedBox(height: 25),
+                MaterialButton(
+                  minWidth: 400,
+                  height: 50,
+                  color: Colors.lightGreenAccent,
+                  elevation: 0,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(7)),
+                  child: Text("Login",
+                      style: TextStyle(
+                          color: Colors.black, fontWeight: FontWeight.bold)),
+                  onPressed: () => {print("Logging in...")},
+                ),
+                SizedBox(height: 25),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text("Don't have an account? "),
+                    GestureDetector(
+                        child: Text("Sign up",
+                            style: TextStyle(fontWeight: FontWeight.bold)))
+                  ],
+                )
+              ],
+            )));
   }
 }
