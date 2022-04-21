7d7b962b848d8dd8b2f0c7b86a96a0cb636131c1
svencarstenrasmusen <sven.the.eagle@gmail.com>
2022-04-21 16:20:08 +0200

    added additional buttons in menuBar with boolean selection highlight

--- lib/dashboard_page.dart
+++ lib/dashboard_page.dart
@@ -16,39 +16,70 @@ class _DashboardPageState extends State<DashboardPage> {
     double width = MediaQuery.of(context).size.width;
     double height = MediaQuery.of(context).size.height;
 
-    return Container(
-      height: height,
-      width: width,
-      child: Row(
-        children: [
-          Expanded(flex: 1, child: menuBar()),
-          Expanded(flex: 5, child: Container())
-        ],
+    return Scaffold(
+      body: Container(
+        height: height,
+        width: width,
+        child: Row(
+          children: [
+            Expanded(flex: 1, child: menuBar()),
+            Expanded(flex: 4, child: Container())
+          ],
+        ),
       ),
     );
   }
 
   Widget menuBar() {
     return Container(
+      padding: const EdgeInsets.fromLTRB(15, 10, 15, 10),
       color: const Color(0xFF0A150F),
-      padding: const EdgeInsets.fromLTRB(10, 10, 10, 10),
       child: Column(
         children: [
-          Text("Phishlify", style: TextStyle(fontSize: 20, color: Colors.white)),
-          menuButton(Icons.analytics, "Dashboard", "/dashboard"),
+          const Text("Phishlify", style: TextStyle(fontSize: 20, color: Colors.white)),
+          const SizedBox(height: 50),
+          Align(
+              alignment: Alignment.centerLeft,
+              child: const Text("GENERAL", style: TextStyle(fontSize: 10, color: Colors.grey))),
+          const SizedBox(height: 10),
+          menuButton(Icons.analytics_outlined, "Dashboard", "/dashboard", true),
+          const SizedBox(height: 10),
+          menuButton(Icons.add_box_outlined, "Campaigns", "/dashboard", false),
+          const SizedBox(height: 10),
+          menuButton(Icons.folder_outlined, "Projects", "/dashboard", false),
+          const SizedBox(height: 10),
+          menuButton(Icons.settings_outlined, "Settings", "/dashboard", false),
+          const SizedBox(height: 50),
+          Align(
+              alignment: Alignment.centerLeft,
+              child: const Text("OTHER OPTIONS", style: TextStyle(fontSize: 10, color: Colors.grey))),
+          const SizedBox(height: 10),
+          menuButton(Icons.article_outlined, "User Guide", "/dashboard", false),
+          const SizedBox(height: 10),
+          menuButton(Icons.description_outlined, "API Document", "/dashboard", false),
         ],
       ),
     );
   }
 
-  Widget menuButton(IconData icon, String text, String routeName) {
+  Widget menuButton(IconData icon, String text, String routeName, bool selected) {
     return MaterialButton(
-      height: 25,
-      color: Colors.lightGreenAccent,
+      height: 40,
+      color: selected? Colors.lightGreenAccent : Color(0xFF0A150F),
       elevation: 0,
       shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(4)),
-      child: Text(text, style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold)),
+          borderRadius: BorderRadius.circular(2)),
+      child: Row(
+        children: [
+          Icon(icon,
+              color: selected? Color(0xFF0A150F) : Colors.grey
+          ),
+          SizedBox(width: 10),
+          Text(text, style: TextStyle(
+              color: selected? Color(0xFF0A150F) : Colors.grey,
+              fontWeight: FontWeight.bold))
+        ],
+      ),
       onPressed: () {
         Navigator.pushNamed(context, routeName);
       },
