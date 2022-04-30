d4ad952892efebff157665a485d6e294762348fc
Naaima <naaima9323@gmail.com>
2022-04-30 23:48:29 +0200

    Added campaigns endpoint

--- backend/app.js
+++ backend/app.js
@@ -1,19 +1,29 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-
 const connection = require('./db');
-
 
 // gives you projects against email 
 app.get('/projects/:email', (request, response) => {
 
     const email = request.params.email;
-    console.log(email);
     let sql_query = `Select * from user
     JOIN projects ON user.userId = projects.userId
     WHERE user.email="${email}"`
 
+    connection.query(sql_query, function (err, results) {
+        if (err) throw err;
+        console.log('result ', results)
+        response.send(results);
+    });
+});
+
+
+// gives campaigns against projectid 
+app.get('/campaigns/:projectid', (request, response) => {
+
+    const projectId = request.params.projectid;
+    let sql_query = `SELECT * FROM campaigns where projectId="${projectId}"`
     connection.query(sql_query, function (err, results) {
         if (err) throw err;
         console.log('result ', results)
