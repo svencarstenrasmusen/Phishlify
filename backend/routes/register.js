b68e8135d608754f2af7a37bdf5d1adb92ef48fb
svencarstenrasmusen <sven.the.eagle@gmail.com>
2022-05-04 13:28:01 +0200

    no longer require user id, it is auto incremented

--- backend/routes/register.js
+++ backend/routes/register.js
@@ -4,7 +4,6 @@ const connection = require('../db');
 
 router.post('/', (req, res) => {
     console.log('register 1 ', req.body);
-    const userId = req.body.userId;
     const name = req.body.name;
     const email = req.body.email;
     const password = req.body.password;
@@ -14,16 +13,16 @@ router.post('/', (req, res) => {
     connection.query(userExists, function (err, results) {
         if (err) throw err;
         if (results.length > 0) {
-            res.send('User Alreadry Exists !')
+            res.send('User Already Exists!')
         }
         else {
-            if(userId !== '' && name !== '' && email !== '' && password !==''){
-                let sql_query = `INSERT INTO user
-                VALUES (${userId}, "${name}", "${email}", ${password})`;
+            if(name !== '' && email !== '' && password !==''){
+                let sql_query = `INSERT INTO user (name, email, password)
+                VALUES ("${name}", "${email}", ${password})`;
                 connection.query(sql_query, function (err, results) {
                     if (err) throw err;
                     console.log('result ', results)
-                    res.send("User Inserted ! ");
+                    res.send("User Inserted!");
                 });
             } else {
                 res.send('Fields cannot be empty!')
