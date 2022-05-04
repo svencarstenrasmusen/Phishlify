eb2c17fb54c825b7cdd03cac3e5915bd0a0c9b8f
svencarstenrasmusen <sven.the.eagle@gmail.com>
2022-05-04 13:01:58 +0200

    changed all IDs to auto increment

--- database/Dump20220424.sql
+++ database/Dump20220424.sql
@@ -23,7 +23,7 @@ DROP TABLE IF EXISTS `campaigns`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `campaigns` (
-  `campaignId` int NOT NULL,
+  `campaignId` int NOT NULL AUTO_INCREMENT,
   `name` varchar(45) DEFAULT NULL,
   `description` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`campaignId`)
@@ -38,7 +38,7 @@ DROP TABLE IF EXISTS `projects`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `projects` (
-  `projectId` int NOT NULL,
+  `projectId` int NOT NULL AUTO_INCREMENT,
   `name` varchar(45) DEFAULT NULL,
   `peronInCharge` varchar(45) DEFAULT NULL,
   `domain` varchar(45) DEFAULT NULL,
