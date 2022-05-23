c6cb1e525760ad14c4e3457b28f3c90f567c0790
svencarstenrasmusen <sven.the.eagle@gmail.com>
2022-05-23 11:37:53 +0200

    new repo

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,11 @@
 const mysql = require("mysql");
 
 const connection = mysql.createConnection({
-    host: 'localhost',
+    host: '127.0.0.1',
     database: 'phishlify',
     user: 'root',
-    password: '87654321'
+    password: '1flockenfutter!1',
+    port: 3307
 });
 
 module.exports = connection;
