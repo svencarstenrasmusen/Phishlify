4cdd005d2df03472abebb065664c6dc468f8c46b
SvenRasmusen <sven@home.lu>
2022-05-31 20:48:56 +0200

    added delete confirm dialog

--- lib/custom_widgets/campaign_tile.dart
+++ lib/custom_widgets/campaign_tile.dart
@@ -1,16 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:phishing_framework/data/models.dart';
+import 'package:phishing_framework/data/dataprovider.dart';
 
 class CampaignTile extends StatefulWidget {
+  final Function(Campaign) removeCallback;
   final Campaign campaign;
 
-  CampaignTile({required this.campaign});
+  CampaignTile({required this.campaign, required this.removeCallback});
 
   @override
   _CampaignTileState createState() => _CampaignTileState();
 }
 
 class _CampaignTileState extends State<CampaignTile> {
+
+  DataProvider dataProvider = DataProvider();
+
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -27,8 +32,17 @@ class _CampaignTileState extends State<CampaignTile> {
               child: Row(
                 children: [
                   Spacer(),
-                  Expanded(child: Icon(Icons.edit)),
-                  Expanded(child: Icon(Icons.delete_forever)),
+                  Expanded(child: IconButton(
+                    onPressed: () {
+                    },
+                    icon: Icon(Icons.edit),
+                  )),
+                  Expanded(child: IconButton(
+                    onPressed: () {
+                      showConfirmDeleteDialog();
+                    },
+                    icon: Icon(Icons.delete_forever),
+                  )),
                   Spacer(),
                 ],
               ),
@@ -36,6 +50,41 @@ class _CampaignTileState extends State<CampaignTile> {
           ],
         ),
       ),
+    );
+  }
+
+  showConfirmDeleteDialog() async {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(Icons.warning, size: 40, color: Colors.red),
+              SizedBox(height: 20),
+              Text("Are you sure you want to delete the campaign: ${widget.campaign.name}?")
+            ],
+          ),
+          actions: [
+            MaterialButton(
+              child: Text('Cancel'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            MaterialButton(
+              child: Text('Yes, delete', style: TextStyle(color: Colors.white)),
+              color: Colors.red,
+              onPressed: () {
+                widget.removeCallback(widget.campaign);
+                Navigator.of(context).pop();
+              },
+            )
+          ],
+        );
+      }
     );
   }
 }
